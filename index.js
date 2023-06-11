c0973f35b77b77c80ffcde89087ac00aa2b906e5
aminatol <aminatoluwo@gmail.com>
2023-06-11 14:53:34 -0400

    input javacript variable for quiz questions

---
+++ index.js
@@ -0,0 +1,11 @@
+let questions= document.getElementById('trivia-question.numb')
+questions=
+    {
+        question:"This is the national flag of which nation",
+        option:['A. Bangladesh',"B. Jordan", "C. India", "D. Sri Lanka"],
+        correct:3,
+    
+    },
+{question:"What is the capitol of France?",
+
+}
